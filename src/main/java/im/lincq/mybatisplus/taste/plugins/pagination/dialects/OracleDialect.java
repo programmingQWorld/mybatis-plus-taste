d2ef581b40c30bc2870da3e2558ba6c57460e37a
lincq <aawoshilcq@163.com>
2019-06-30 12:02:34 +0800

    升级1.2.15 OracleDialect分页语句拼接纠正.

--- src/main/java/im/lincq/mybatisplus/taste/plugins/pagination/dialects/OracleDialect.java
+++ src/main/java/im/lincq/mybatisplus/taste/plugins/pagination/dialects/OracleDialect.java
@@ -11,8 +11,8 @@ public class OracleDialect implements IDialect {
     public String buildPaginationSql(String originalSql, int offset, int limit) {
         StringBuilder sql = new StringBuilder();
         sql.append("SELECT * FROM ( SELECT TMP.*, ROWNUM ROW_ID FROM ( ");
-        sql.append(originalSql).append(" ) TMP WHERE ROWNUM <=").append(offset);
-        sql.append(") WHERE ROW_ID > ").append(limit);
+        sql.append(originalSql).append(" ) TMP WHERE ROWNUM <=").append((offset >=1)?(offset*limit):limit);
+        sql.append(") WHERE ROW_ID > ").append(offset);
         return sql.toString();
     }
 }
