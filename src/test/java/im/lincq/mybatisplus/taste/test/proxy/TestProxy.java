8fbf6a4e39f5c7561e2424bddf88d0f20cb13982
lincq <aawoshilcq@163.com>
2019-07-15 17:16:35 +0800

    添加 mybatis 代理模式原理测试用例 hubin 2016/7/6 12:49

--- src/test/java/im/lincq/mybatisplus/taste/test/proxy/TestProxy.java
+++ src/test/java/im/lincq/mybatisplus/taste/test/proxy/TestProxy.java
@@ -4,17 +4,73 @@ import im.lincq.mybatisplus.taste.test.proxy.mapper.IUserMapper;
 import im.lincq.mybatisplus.taste.test.proxy.mapper.User;
 import im.lincq.mybatisplus.taste.test.proxy.mapper.UserMapperImpl;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
 /**
  * <p>
- *     mybatis 执行原理测试
+ * mybatis 执行原理测试
  * </p>
  */
 public class TestProxy {
 
-    public static void main ( String[] rags ) {
+    public static void main(String[] rags) {
+
+        /**
+         * 代理方式一
+         */
         IUserMapper userMapper = MapperProxyFactory.getMapper(IUserMapper.class);
         User user = userMapper.selectById(1L);
-        userMapper.deleteById(1L);
         System.err.println((user == null) ? "代理失败" : user.getName());
+
+        try {
+            Thread.sleep(3000L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            System.err.println("\n");
+        }
+
+        /**
+         * 代理方式二
+         */
+        MyMapperProxy<IUserMapper> userDaoProxy = new MyMapperProxy<IUserMapper>();
+        IUserMapper mapper = userDaoProxy.bind(new UserMapperImpl());
+        User user1 = mapper.selectById(1L);
+        System.err.println(user1 == null ? "代理失败" : user1.getName() + user1.getAge());
     }
+}
+
+/**
+ * 动态代理方式二
+ *
+ * @param <T>
+ */
+class MyMapperProxy<T extends IUserMapper> implements InvocationHandler {
+
+    private T t;
+
+    @SuppressWarnings("unchecked")
+    public T bind(T t) {
+        this.t = t;
+        return (T) Proxy.newProxyInstance(t.getClass().getClassLoader(), t.getClass().getInterfaces(), this);
+    }
+
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        Object rst = null;
+        before();
+        rst = method.invoke(t, args);
+        after();
+        return rst;
+    }
+
+    private void before () {
+        System.out.println("before ... ");
+    }
+
+    private void after () {
+        System.out.println("after ... ");
+    }
+
 }
